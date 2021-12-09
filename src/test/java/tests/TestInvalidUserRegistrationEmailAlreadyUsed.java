6f4ae9385334d54f2303565ce7e80d5bd8bad0f9
matteobiagiola <biagiolamatteo2@gmail.com>
2021-12-09 19:42:15 +0100

    test suite fixed

--- src/test/java/tests/TestInvalidUserRegistrationEmailAlreadyUsed.java
+++ src/test/java/tests/TestInvalidUserRegistrationEmailAlreadyUsed.java
@@ -12,27 +12,27 @@ public class TestInvalidUserRegistrationEmailAlreadyUsed extends BaseTest {
   @Test
   public void testInvalidUserRegistrationEmailAlreadyUsed() throws InterruptedException {
 
-    WebElement blueTShirtLink = driver.findElement(By.xpath("//h3[contains(text(),'Blue t-shirt')]"));
+    WebElement blueTShirtLink = driver.findElement(By.xpath("//h3[contains(text(),'Blue T-shirt')]"));
     blueTShirtLink.click();
 
-    WebElement increaseQuantityButton = driver.findElement(By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]"));
+    WebElement increaseQuantityButton = driver.findElement(By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/span[2]/button[1]"));
     increaseQuantityButton.click();
     increaseQuantityButton.click();
-    WebElement largeSizeInput = driver.findElement(By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/input[1]"));
+    WebElement largeSizeInput = driver.findElement(By.xpath("/html[1]/body[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/label[1]/input[1]"));
     largeSizeInput.click();
-    WebElement addToCartButton = driver.findElement(By.xpath("//button[contains(text(),'add to cart')]"));
+    WebElement addToCartButton = driver.findElement(By.xpath("//button[contains(text(),'Add to cart')]"));
     addToCartButton.click();
 
-    WebElement notificationMessage = driver.findElement(By.xpath("//div[@id='notify_msg']"));
+    WebElement notificationMessage = driver.findElement(By.xpath("//div[@id='notify_message']"));
     assertEquals("Cart successfully updated", notificationMessage.getText());
 
-    WebElement cartIcon = driver.findElement(By.xpath("//a[@class='menu']"));
+    WebElement cartIcon = driver.findElement(By.xpath("//a[@class='menu-btn']"));
     cartIcon.click();
 
-    WebElement checkoutButton = driver.findElement(By.xpath("//a[@class='btn']"));
+    WebElement checkoutButton = driver.findElement(By.xpath("//a[@class='btn btn-default']"));
     checkoutButton.click();
 
-    WebElement payNowButton = driver.findElement(By.xpath("//a[@class='btn-default']"));
+    WebElement payNowButton = driver.findElement(By.xpath("//a[@class='btn btn-default']"));
     payNowButton.click();
 
     String randomEmail = "test@test.it";
@@ -45,7 +45,7 @@ public class TestInvalidUserRegistrationEmailAlreadyUsed extends BaseTest {
     String phoneNumber = "123456789";
     String password = "password";
 
-    WebElement newCustomerEmailAddressInput = driver.findElement(By.xpath("//input[@id=\"shipemail\"]"));
+    WebElement newCustomerEmailAddressInput = driver.findElement(By.xpath("//input[@id=\"shipEmail\"]"));
     WebElement newCustomerFirstNameInput = driver.findElement(By.xpath("//input[@id=\"shipFirstname\"]"));
     WebElement newCustomerLastNameInput = driver.findElement(By.xpath("//input[@id=\"shipLastname\"]"));
     WebElement newCustomerAddress1Input = driver.findElement(By.xpath("//input[@id=\"shipAddr1\"]"));
@@ -53,6 +53,7 @@ public class TestInvalidUserRegistrationEmailAlreadyUsed extends BaseTest {
     WebElement newCustomerStateInput = driver.findElement(By.xpath("//input[@id=\"shipState\"]"));
     WebElement newCustomerPostCodeInput = driver.findElement(By.xpath("//input[@id=\"shipPostcode\"]"));
     WebElement newCustomerPasswordInput = driver.findElement(By.xpath("//input[@id='newCustomerPassword']"));
+    WebElement newCustomerPhoneNumberInput = driver.findElement(By.xpath("//input[@id=\"shipPhoneNumber\"]"));
     WebElement createAccountButton = driver.findElement(By.xpath("//a[@id='createCustomerAccount']"));
 
     newCustomerEmailAddressInput.sendKeys(randomEmail);
@@ -62,6 +63,7 @@ public class TestInvalidUserRegistrationEmailAlreadyUsed extends BaseTest {
     newCustomerCountryInput.sendKeys(country);
     newCustomerStateInput.sendKeys(state);
     newCustomerPostCodeInput.sendKeys(postCode);
+    newCustomerPhoneNumberInput.sendKeys(phoneNumber);
     newCustomerPasswordInput.sendKeys(password);
     createAccountButton.click();
 
